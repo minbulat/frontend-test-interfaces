6399c5e6e8d534151deca9aa0db4355268881860
minbulat <b@minbulat.ru>
2019-08-10 16:11:12 +0300

    Error alert component

---
+++ src/components/error.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+    .error
+        p(v-for="err in error") {{ error }}
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        name: 'error',
+        props: {
+            error: [],
+        },
+    });
+</script>
+
+<style scoped lang="stylus">
+    .error
+        color white
+        background crimson
+</style>
